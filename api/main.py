aebba082ae70a7cda9329bf00a60ca0ccaae8478
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-06-29 11:36:05 +0530

    new changes

--- api/main.py
+++ api/main.py
@@ -21,6 +21,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # for testing purposes, allow all origins 
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
